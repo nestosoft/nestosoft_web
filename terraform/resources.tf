1cb69880a22a612b408139c55e1051c4b0ad59cf
Ernesto <ernesto@nestosoft.com>
2024-11-06 15:37:13 +0000

    replace null resource with terraform data

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -49,12 +49,13 @@ resource "aws_instance" "main" {
 
 }
 
-resource "null_resource" "webapp" {
+# Replaced null_resource with terraform_data
+resource "terraform_data" "webapp" {
 
-  triggers = {
-    webapp_server_count = length(aws_instance.main.*.id)
-    web_server_names    = join(",", aws_instance.main.*.id)
-  }
+  triggers_replace = [
+    length(aws_instance.main.*.id),
+    join(",", aws_instance.main.*.id)
+  ]
 
   provisioner "file" {
     content = templatefile("./templates/application.config.tpl", {
