520c8b31784aed1999e6155361139880a24efcf9
Ernesto <ernesto@nestosoft.com>
2024-11-13 13:35:00 +0000

    copying netlify to env

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -16,7 +16,7 @@ const Layout = ({}) => {
             <Outlet />
           </div>
           <footer>
-            Hosted in {import.meta.env.VITE_HOST || "local"}
+            Hosted in {import.meta.env.VITE_HOST}
           </footer>
         </div> 
     )
